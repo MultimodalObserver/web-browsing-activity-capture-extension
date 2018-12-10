412f7d931b33cadfaca35bb6a11a035279bb2c1d
abrahamCerda <abraham.cerda@usach.cl>
2018-12-10 15:23:40 -0300

    Mo logo added as icon for the extension

--- js/background.js
+++ js/background.js
@@ -34,14 +34,5 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 				});
 			});
 		}
-		/*
-          var storageChange = changes[key];
-          console.log('Storage key "%s" in namespace "%s" changed. ' +
-                      'Old value was "%s", new value is "%s".',
-                      key,
-                      namespace,
-                      storageChange.oldValue,
-                      storageChange.newValue);
-                      */
     }
 });
